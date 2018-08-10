b50ff5cea559361fe9c5ac7dd216063463238da4
bluelovers <codelovers@users.sourceforge.net>
2018-08-11 03:25:21 +0800

    .

--- bin/pack-jar.ts
+++ bin/pack-jar.ts
@@ -5,17 +5,24 @@ import * as path from 'path';
 import { PKG } from '../lib/util';
 import { PROJECT_PRODUCTION, PROJECT_RELEASES, PROJECT_ROOT } from '../project.config';
 import CrossSpawn = require('cross-spawn');
+import * as fs from 'fs-extra';
+import Promise = require('bluebird');
 
-CrossSpawn(path.join(process.env.JDK_HOME, 'bin/jar'), [
+fs.remove(path.join(PROJECT_PRODUCTION, PKG.name, PKG.name + '.jar'))
+	.then(function ()
+	{
+		return CrossSpawn(path.join(process.env.JDK_HOME, 'bin/jar'), [
 
-	'cvf',
-	path.join(PROJECT_RELEASES, PKG.name + '.jar'),
+			'cvf',
+			path.join(PROJECT_RELEASES, PKG.name + '.jar'),
 
-	'-C',
-	path.join(PROJECT_PRODUCTION, PKG.name),
+			'-C',
+			path.join(PROJECT_PRODUCTION, PKG.name),
 
-	'.',
-], {
-	stdio: 'inherit',
-	cwd: PROJECT_ROOT,
-});
+			'.',
+		], {
+			stdio: 'inherit',
+			cwd: PROJECT_ROOT,
+		});
+	})
+;
